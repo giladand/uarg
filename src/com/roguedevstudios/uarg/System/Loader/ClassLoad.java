dd31138017c84fcd087882bc9eddba625550e9bc
Rivera-Joe <36744600+Rivera-Joe@users.noreply.github.com>
2018-05-05 09:09:22 -0500

    Added new method  ModRun which calls ModCheck. Also created a variable (of string type) "FilePath" which saves the file path of the module within the catch function.

--- src/com/roguedevstudios/uarg/System/Loader/ClassLoad.java
+++ src/com/roguedevstudios/uarg/System/Loader/ClassLoad.java
@@ -1,4 +1,8 @@
 package com.roguedevstudios.uarg.System.Loader;
+/*This class is designed to call a method that searches for a module 
+ * and attempts to retrieve it, if any exist.
+ * The class also stores the file path as a string
+ */
 
 /*
  * @author Joe Rivera
@@ -9,21 +13,27 @@ package com.roguedevstudios.uarg.System.Loader;
 import java.io.File;
 
 public class ClassLoad {
+	public static void ModRun(String[] args) {
+		ModCheck(); // Method Runs ModCheck
+	}
 
-	public static void ModCheck(String[] args) {
+	public static void ModCheck() {
 		File Module = new File("Folder");
 		//declares module as File "Folder"
+		
 		
 		if(Module.exists()) {
 			try {
 				
 				Module.getPath();
-				//retrieves filepath for Module
-				Module.getClass();
+				//retrieves file path for Module
+			    Module.getClass();
 				//retrieves class of Module if any
 			}
 			catch(Exception e) {
-				System.out.println("Cannot find module in FilePath" +Module.getPath());
+				String FilePath = Module.getPath();
+				
+				System.out.println("Cannot find module in FilePath" + " " +FilePath);
 				//Prints error to screen
 			}
 				 
@@ -37,4 +47,6 @@ public class ClassLoad {
 	
 
 }
+
+
 

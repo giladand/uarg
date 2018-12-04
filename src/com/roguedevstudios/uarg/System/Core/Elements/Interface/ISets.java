47a535560952002ada646bf5e3b9194f12144c5a
John Mai <jprebornzer@gmail.com>
2018-12-03 21:30:29 -0600

    UARG-276 #time 10m Updated ISet to reflect Sets

--- src/com/roguedevstudios/uarg/System/Core/Elements/Interface/ISets.java
+++ src/com/roguedevstudios/uarg/System/Core/Elements/Interface/ISets.java
@@ -1,13 +1,15 @@
 package com.roguedevstudios.uarg.System.Core.Elements.Interface;
 
-import java.util.List;
+import java.util.Set;
+
+import com.roguedevstudios.uarg.System.Core.Elements.Sets;
 
 /********************************
-*   ISet Interface		    *
-*   File Name: ISet.java   *
+*   ISet Interface		    	*
+*   File Name: ISet.java   		*
 *                               *
 *   Inversion of Control		*
-*   for Set		 		*
+*   for Set		 				*
 *                               *
 *  ©2017 Rogue Dev Studios, LLC *
 ********************************/
@@ -16,13 +18,12 @@ import java.util.List;
  * Interface representing requirements for Set class
  * 
  * @author John Mai
+ * @param <V>
  * @since 1.0
  */
-public interface ISets {
+public interface ISets<V> {
 
-	void AddSet(String setID);
-	void AddArgsToSet(String setID, String argsID);
-	List<String> GetSet(String setID);
-	Double GetSetValue(String setID);
-	
+	void addSets(Sets<V> set);
+	Set<Sets<V>> getSet();
+	Double getSetValue();
 }
